adef82079f4500dc196cba26984d80aae48d66e8
sebasukodo <231564377+sebasukodo@users.noreply.github.com>
2026-01-14 15:30:56 +0100

    create config file

---
+++ internal/config/config.go
@@ -0,0 +1,79 @@
+package config
+
+import (
+	"encoding/json"
+	"os"
+	"path/filepath"
+)
+
+const configFileName = "/.gatorconfig.json"
+
+type Config struct {
+	DBURL           string `json:"db_url"`
+	CurrentUserName string `json:"current_user_name"`
+}
+
+func Read() (Config, error) {
+
+	filePath, err := getConfigFilePath()
+	if err != nil {
+		return Config{}, err
+	}
+
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		return Config{}, err
+	}
+
+	var cfg Config
+
+	if err := json.Unmarshal(data, &cfg); err != nil {
+		return Config{}, err
+	}
+
+	return cfg, nil
+
+}
+
+func (cfg *Config) SetUser(userName string) error {
+
+	cfg.CurrentUserName = userName
+
+	if err := write(*cfg); err != nil {
+		return err
+	}
+
+	return nil
+
+}
+
+func getConfigFilePath() (string, error) {
+	filePath, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+
+	fullPath := filepath.Join(filePath, configFileName)
+
+	return fullPath, nil
+}
+
+func write(cfg Config) error {
+
+	filePath, err := getConfigFilePath()
+	if err != nil {
+		return err
+	}
+
+	data, err := json.Marshal(cfg)
+	if err != nil {
+		return err
+	}
+
+	if err := os.WriteFile(filePath, data, 0644); err != nil {
+		return err
+	}
+
+	return nil
+
+}
--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/sebasukodo/blog-aggregator/internal/config"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+
+	cfg, err := config.Read()
+	if err != nil {
+		log.Fatalf("error reading config file: %v", err)
+	}
+
+	if err = cfg.SetUser("sebasukodo"); err != nil {
+		log.Fatalf("error writing config file: %v", err)
+	}
+
+	cfg, err = config.Read()
+	if err != nil {
+		log.Fatalf("error reading config file: %v", err)
+	}
+
+	fmt.Println("DB URL:", cfg.DBURL)
+	fmt.Println("Current user:", cfg.CurrentUserName)
+
 }
